9ae171d82dd9166a96f14cda03c986385f5432be
Dan Neal <dan.r.neal@gmail.com>
2019-06-04 12:46:45 -0700

    add list owners via forms, more isolated tests

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -61,4 +61,8 @@ class MyListsTest(FunctionalTest):
 
         # She logs out. The "My lists" option disappears
         self.browser.find_element_by_link_text('Log out').click()
-        self.wait_for(lambda: self.assertNotContains('My lists'))
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text('My lists'), []
+            )
+        )
