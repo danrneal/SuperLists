ead52cf28fab5dda94da22fa3749b8f05baaf241
Dan Neal <dan.r.neal@gmail.com>
2019-06-06 17:44:22 -0700

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import os
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-from .server_tools import reset_database
+from .management.commands.create_session import create_pre_authenticated_session
+from .server_tools import create_session_on_server, reset_database
 
 MAX_WAIT = 10
 SCREEN_DUMP_LOCATION = os.path.join(
@@ -37,6 +39,16 @@ class FunctionalTest(StaticLiveServerTestCase):
     def _test_has_failed(self):
         return any(error for (method, error) in self._outcome.errors)
 
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{class_}.{method}.window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            class_=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
+
     def take_screenshot(self):
         filename = self._get_filename() + '.png'
         print('screenshotting to', filename)
@@ -48,15 +60,20 @@ class FunctionalTest(StaticLiveServerTestCase):
         with open(filename, 'w') as f:
             f.write(self.browser.page_source)
 
-    def _get_filename(self):
-        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
-        return '{folder}/{class_}.{method}.window{windowid}-{timestamp}'.format(
-            folder=SCREEN_DUMP_LOCATION,
-            class_=self.__class__.__name__,
-            method=self._testMethodName,
-            windowid=self._windowid,
-            timestamp=timestamp
-        )
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        ## to set a cookie we neet to first visit the domain, 404 pages load
+        ## the quickest!
+        self.browser.get(self.live_server_url + '/404_no_such_url')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/'
+        ))
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
